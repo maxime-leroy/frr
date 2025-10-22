maxime-leroy/frr

commit dc7b3caefbd8baccb7fc3787a774e78d1a96636f

zebra: Add one-shot thread to recheck speed
There are certain interfaces that when brought up and we receive
the netlink notification about it, the speed of the interface is
not set correctly.  This creates a one-shot thread that will
wait 15 seconds and then requery the speed and if it is different
it will renotify the running daemons.

The kernel should notify us on speed changes, unfortunately this
is not done currently via a netlink message as you would think.
As I understand it there is some in-fighting about the proper
way to approach this issue and due to the way the kernel release
cycle works we are a ways off from getting this fixed.  This
is a `hack` to make us work correctly while we wait for the
true answer.

Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

zebra/interface.c
@@ -57,7 +57,28 @@ DEFINE_HOOK(zebra_if_extra_info, (struct vty *vty, struct interface *ifp),
 DEFINE_HOOK(zebra_if_config_wr, (struct vty *vty, struct interface *ifp),
 				(vty, ifp))
 
+
 static void if_down_del_nbr_connected(struct interface *ifp);
+
+static int if_zebra_speed_update(struct thread *thread)
+{
+	struct interface *ifp = THREAD_ARG(thread);
+	struct zebra_if *zif = ifp->info;
+	uint32_t new_speed;
+
+	zif->speed_update = NULL;
+
+	new_speed = kernel_get_speed(ifp);
+	if (new_speed != ifp->speed) {
+		zlog_info("%s: %s old speed: %u new speed: %u",
+			  __PRETTY_FUNCTION__, ifp->name,
+			  ifp->speed, new_speed);
+		ifp->speed = new_speed;
+		if_add_update(ifp);
+	}
+
+	return 1;
+}
 
 static void zebra_if_node_destroy(route_table_delegate_t *delegate,
 				  struct route_table *table,
@@ -119,6 +140,16 @@ static int if_zebra_new_hook(struct interface *ifp)
 		route_table_init_with_delegate(&zebra_if_table_delegate);
 
 	ifp->info = zebra_if;
+
+	/*
+	 * Some platforms are telling us that the interface is
+	 * up and ready to go.  When we check the speed we
+	 * sometimes get the wrong value.  Wait a couple
+	 * of seconds and ask again.  Hopefully it's all settled
+	 * down upon startup.
+	 */
+	thread_add_timer(zebrad.master, if_zebra_speed_update,
+			 ifp, 15, &zebra_if->speed_update);
 	return 0;
 }
 
@@ -140,6 +171,8 @@ static int if_zebra_delete_hook(struct interface *ifp)
 		rtadv = &zebra_if->rtadv;
 		list_delete_and_null(&rtadv->AdvPrefixList);
 #endif /* HAVE_RTADV */
+
+		THREAD_OFF(zebra_if->speed_update);
 
 		XFREE(MTYPE_TMP, zebra_if);
 	}

zebra/if_netlink.c
@@ -387,6 +387,11 @@ static int get_iflink_speed(const char *ifname)
 	return (ecmd.speed_hi << 16) | ecmd.speed;
 }
 
+uint32_t kernel_get_speed(struct interface *ifp)
+{
+	return get_iflink_speed(ifp->name);
+}
+
 static int netlink_extract_bridge_info(struct rtattr *link_data,
 				       struct zebra_l2info_bridge *bridge_info)
 {

zebra/interface.h
@@ -273,6 +273,8 @@ struct zebra_if {
 	/* Link fields - for sub-interfaces. */
 	ifindex_t link_ifindex;
 	struct interface *link;
+
+	struct thread *speed_update;
 };
 
 DECLARE_HOOK(zebra_if_extra_info, (struct vty *vty, struct interface *ifp),

zebra/rt.h
@@ -98,6 +98,7 @@ extern void kernel_lsp_pass_fail(zebra_lsp_t *lsp,
 
 extern int mpls_kernel_init(void);
 
+extern uint32_t kernel_get_speed(struct interface *ifp);
 extern int kernel_get_ipmr_sg_stats(struct zebra_vrf *zvrf, void *mroute);
 extern int kernel_add_vtep(vni_t vni, struct interface *ifp,
 			   struct in_addr *vtep_ip);

zebra/rt_socket.c
@@ -473,4 +473,9 @@ extern int kernel_interface_set_master(struct interface *master,
 	return 0;
 }
 
+uint32_t kernel_get_speed(struct interface *ifp)
+{
+	return ifp->speed;
+}
+
 #endif /* !HAVE_NETLINK */

commit d22e36869524fbbe0b0a3e9f9f9bd51241b149bc

zebra: schedule speed-query timer only when the interface exists
The 15-second timer used to re-query interface speed is currently
scheduled in if_zebra_new_hook() for every newly created
interface. However, at that point the interface may not yet exist in the
OS, and in some cases it may never be created.

Because of this, the speed query will usually
fail (e.g. INTERFACE_SPEED_ERROR_READ) since the interface doesn't
exist. There is also a race condition: even if the interface is created,
the timer may run before the RTM_NEWLINK message is processed.

As a result, ifp->ifp_index can remain IFINDEX_INTERNAL (0). When
if_add_update() calls zebra_ns_link_ifp(), the interface tree is updated
with this incorrect index. If this happens for multiple interfaces, the
tree can end up with duplicate keys, eventually causing a zebra crash.

To avoid this, the speed-update timer is now scheduled only when a
RTM_NEWLINK is received. The scheduling logic is moved from
if_zebra_new_hook() to zebra_if_dplane_ifp_handling() and it runs only
when the interface does not yet exist (ifp == NULL).

Fixes: dc7b3caefbd8 ("zebra: Add one-shot thread to recheck speed")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

zebra/interface.c
@@ -156,18 +156,6 @@ static int if_zebra_new_hook(struct interface *ifp)
 
 	ifp->info = zebra_if;
 
-	/*
-	 * Some platforms are telling us that the interface is
-	 * up and ready to go.  When we check the speed we
-	 * sometimes get the wrong value.  Wait a couple
-	 * of seconds and ask again.  Hopefully it's all settled
-	 * down upon startup.
-	 */
-	zebra_if->speed_update_count = 0;
-	event_add_timer(zrouter.master, if_zebra_speed_update, ifp, 15,
-			&zebra_if->speed_update);
-	event_ignore_late_timer(zebra_if->speed_update);
-
 	return 0;
 }
 
@@ -1986,6 +1974,19 @@ static void zebra_if_dplane_ifp_handling(struct zebra_dplane_ctx *ctx)
 			if_update_state_mtu6(ifp, mtu);
 			if_update_state_metric(ifp, 0);
 			if_update_state_speed(ifp, kernel_get_speed(ifp, NULL));
+
+			/*
+			 * Some platforms are telling us that the interface is
+			 * up and ready to go.  When we check the speed we
+			 * sometimes get the wrong value.  Wait a couple
+			 * of seconds and ask again.  Hopefully it's all settled
+			 * down upon startup.
+			 */
+			zif->speed_update_count = 0;
+			event_add_timer(zrouter.master, if_zebra_speed_update, ifp, 15,
+					&zif->speed_update);
+			event_ignore_late_timer(zif->speed_update);
+
 			ifp->ptm_status = ZEBRA_PTM_STATUS_UNKNOWN;
 			ifp->txqlen = dplane_ctx_get_intf_txqlen(ctx);
 